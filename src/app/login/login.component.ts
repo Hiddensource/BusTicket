ddbbfecb605a69cba6c7ccd8d4218afe0299604a
Deepak <deepak@Triconinfotech.com>
2018-10-26 16:51:15 +0530

    refractor_1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,46 +2,46 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import {} from '@types/googlemaps';
-import {DataService} from 'src/app/services/data.service'
-import { HttpClient} from '@angular/common/http';
+import { } from '@types/googlemaps';
+import { DataService } from 'src/app/services/data.service'
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
 
   templateUrl: './login.component.html',
- 
+
   styleUrls: ['./login.component.css']
 })
 
 export class LoginComponent implements OnInit {
 
 
- constructor(private router: Router ,
-              private route: ActivatedRoute,
-              private http: HttpClient,
-              private dataService : DataService) { }
+  constructor(private router: Router,
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private dataService: DataService) { }
 
   ngOnInit() {
- 
+
     google.maps.event.addDomListener(window, 'click', this.initialize);
-    
-    var radio_button1=document.getElementById("radio1");
-    var radio_button2=document.getElementById("radio2");
-    var arrival=<HTMLInputElement>document.getElementById("arrival");
-   
-    radio_button1.addEventListener("click",function(){
-      arrival.disabled=true;
-      arrival.value="dd/mm/yyyy";
+
+    var radio_button1 = document.getElementById("One-way");
+    var radio_button2 = document.getElementById("Two-way");
+    var arrival = <HTMLInputElement>document.getElementById("arrival");
+
+    radio_button1.addEventListener("click", function () {
+      arrival.disabled = true;
+      arrival.value = "dd/mm/yyyy";
     });
 
-    radio_button2.addEventListener("click",function(){
-      arrival.disabled=false; 
+    radio_button2.addEventListener("click", function () {
+      arrival.disabled = false;
     });
 
-    if(localStorage.getItem("source")&&localStorage.getItem("destination")){
-      var s=<HTMLInputElement>document.getElementById("source");
-      var d=<HTMLInputElement>document.getElementById("destination");
+    if (localStorage.getItem("source") && localStorage.getItem("destination")) {
+      var s = <HTMLInputElement>document.getElementById("source");
+      var d = <HTMLInputElement>document.getElementById("destination");
       s.value = localStorage.getItem("source");
       d.value = localStorage.getItem("destination");
     }
@@ -51,25 +51,23 @@ export class LoginComponent implements OnInit {
   initialize() {
     var options = {
       types: ['(cities)'],
-      componentRestrictions: {country: "in"}
+      componentRestrictions: { country: "in" }
     };
 
-    var input = document.getElementById('source');
-    var input1 = document.getElementById('destination');
-    var autocomplete = new google.maps.places.Autocomplete((<HTMLInputElement>input),
-    options);
-    var autocomplete1 = new google.maps.places.Autocomplete((<HTMLInputElement>input1),
-    options);
+    var source = document.getElementById('source');
+    var destination = document.getElementById('destination');
+    var source_autocomplete = new google.maps.places.Autocomplete((<HTMLInputElement>source),
+      options);
+    var destination_autocomplete = new google.maps.places.Autocomplete((<HTMLInputElement>destination),
+      options);
   }
- 
-  //on click of submit button
-  //validate input fields
-  validation(){
 
-    var s=<HTMLInputElement>document.getElementById("source");
-    var d=<HTMLInputElement>document.getElementById("destination");
-    var t=<HTMLInputElement>document.getElementById("departure");
-    var r=<HTMLInputElement>document.getElementById("arrival");
+  empty_field_validation() {
+
+    var s = <HTMLInputElement>document.getElementById("source");
+    var d = <HTMLInputElement>document.getElementById("destination");
+    var t = <HTMLInputElement>document.getElementById("departure");
+    var r = <HTMLInputElement>document.getElementById("arrival");
     var auto_source = s.value.split(",");
     s.value = auto_source[0];
 
@@ -80,70 +78,64 @@ export class LoginComponent implements OnInit {
     DataService.JSONObj.destination = d.value.toLowerCase();
     DataService.JSONObj.date = t.value;
     DataService.JSONObj.return_date = r.value;
-    localStorage.setItem("source",s.value);
-    localStorage.setItem("destination",d.value);
-  
+    localStorage.setItem("source", s.value);
+    localStorage.setItem("destination", d.value);
+
     var today = new Date();
     var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
+    var mm = today.getMonth() + 1; //January is 0!
     var yyyy = today.getFullYear();
     var cdd = String(dd);
     var cmm = String(mm);
     var today_date;
 
-    if(dd<10) {
-      cdd = '0'+dd;
-    } 
-    
-    if(mm<10) {
-      cmm = '0'+mm;
-    } 
-    
+    if (dd < 10) {
+      cdd = '0' + dd;
+    }
+
+    if (mm < 10) {
+      cmm = '0' + mm;
+    }
+
     today_date = yyyy + '-' + cmm + '-' + cdd;
 
-    //if all fields are filled
-    if((DataService.JSONObj.source == "") || (DataService.JSONObj.destination =="") || (DataService.JSONObj.date =="") )
-    {
-    
-      var h=<HTMLBodyElement>document.getElementById("h");
-      h.innerText="* Please fill all the fields";
+
+    if ((DataService.JSONObj.source == "") || (DataService.JSONObj.destination == "") || (DataService.JSONObj.date == "")) {
+
+      var errorBox = <HTMLBodyElement>document.getElementById("errorBox");
+      errorBox.innerText = "* Please fill all the fields";
       this.router.navigateByUrl('login');
-    
+
     }
 
 
+    else {
 
-
-    //if source is same as destination
-    else{
-
-      if((DataService.JSONObj.source ) == (DataService.JSONObj.destination ))
-      {
-        var h=<HTMLBodyElement>document.getElementById("h");
-        h.innerText="* Destination cannot be same as source";
+      if ((DataService.JSONObj.source) == (DataService.JSONObj.destination)) {
+        var errorBox = <HTMLBodyElement>document.getElementById("errorBox");
+        errorBox.innerText = "* Destination cannot be same as source";
         this.router.navigateByUrl('login');
       }
 
-      //if entered date is less than todays date
-      else{
+      else {
 
-        if(today_date > DataService.JSONObj.date)
-        {
-          var h=<HTMLBodyElement>document.getElementById("h");
-          h.innerText="* Departure date can not be past date";
+        if (today_date > DataService.JSONObj.date) {
+          var errorBox = <HTMLBodyElement>document.getElementById("errorBox");
+          errorBox.innerText = "* Departure date can not be past date";
           this.router.navigateByUrl('login');
         }
-        //if everything is fine navigate to next page
-        else{
+
+
+        else {
           DataService.guarding = true;
           this.router.navigateByUrl('login/dashboard');
-          DataService.flag=0;
+          DataService.flag = 0;
         }
 
-      } 
+      }
 
     }
 
   }
-  
+
 }
--- src/app/sorting/sorting.component.ts
+++ src/app/sorting/sorting.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {DataService} from 'src/app/services/data.service';
-import {DashboardComponent} from 'src/app/dashboard/dashboard.component';
-import {RouterModule, Router, ActivatedRoute } from '@angular/router';
+import { DataService } from 'src/app/services/data.service';
+import { DashboardComponent } from 'src/app/dashboard/dashboard.component';
+import { RouterModule, Router, ActivatedRoute } from '@angular/router';
 var sort_data;
 
 @Component({
@@ -12,80 +12,80 @@ var sort_data;
 
 export class SortingComponent implements OnInit {
 
- 
-source=DataService.JSONObj.source;
-destination=DataService.JSONObj.destination;
-date=DataService.JSONObj.date;
- 
+
+  source = DataService.JSONObj.source;
+  destination = DataService.JSONObj.destination;
+  date = DataService.JSONObj.date;
+
   constructor(private router: Router) { }
 
   ngOnInit() {
     sort_data = DashboardComponent.dataObj;
   }
- 
-  sort_ascending(sort_element){
-    
-  sort_data.sort(this.sortByPropertyAsc(sort_element));
-  DataService.dataSort = sort_data;
-  this.router.navigateByUrl('login/dashboard');
-  
-}
 
-sort_descending(sort_element){
-    
-  sort_data.sort(this.sortByPropertyDesc(sort_element));
-  DataService.dataSort = sort_data;
-  this.router.navigateByUrl('login/dashboard');
-  
-}
+  sort_ascending(sort_element) {
 
-sortByPropertyAsc = function (property) {
-
-  return function (x, y) {
-    var x_element = x[property];
-    var y_element = y[property];
-    if( property == "duration"){
-     
-      x_element = x_element.replace('m',''); 
-      y_element = y_element.replace('m',''); 
-   
-      var res_x = x_element.split("h");
-      var res_y = y_element.split("h");
-
-      x_element = (parseInt(res_x[0])*60)+parseInt(res_x[1]);
-      y_element = (parseInt(res_y[0])*60)+parseInt(res_y[1]);
-    }
-    x_element = x_element.toString().replace(':',''); 
-    y_element = y_element.toString().replace(':',''); 
-   
-      return ((parseFloat(x_element) === parseFloat(y_element)) ? 0 : ((parseFloat(x_element) > parseFloat(y_element)) ? 1 : -1));
+    sort_data.sort(this.sortByPropertyAsc(sort_element));
+    DataService.dataSort = sort_data;
+    this.router.navigateByUrl('login/dashboard');
 
   }
-}  
 
-sortByPropertyDesc = function (property) {
+  sort_descending(sort_element) {
 
-  return function (x, y) {
-    var x_element = x[property];
-    var y_element = y[property];
-    if( property == "duration"){
-     
-      x_element = x_element.replace('m',''); 
-      y_element = y_element.replace('m',''); 
-   
-      var res_x = x_element.split("h");
-      var res_y = y_element.split("h");
+    sort_data.sort(this.sortByPropertyDesc(sort_element));
+    DataService.dataSort = sort_data;
+    this.router.navigateByUrl('login/dashboard');
 
-      x_element = (parseInt(res_x[0])*60)+parseInt(res_x[1]);
-      y_element = (parseInt(res_y[0])*60)+parseInt(res_y[1]);
+  }
+
+  sortByPropertyAsc = function (property) {
+
+    return function (x, y) {
+      var x_element = x[property];
+      var y_element = y[property];
+      if (property == "duration") {
+
+        x_element = x_element.replace('m', '');
+        y_element = y_element.replace('m', '');
+
+        var res_x = x_element.split("h");
+        var res_y = y_element.split("h");
+
+        x_element = (parseInt(res_x[0]) * 60) + parseInt(res_x[1]);
+        y_element = (parseInt(res_y[0]) * 60) + parseInt(res_y[1]);
+      }
+      x_element = x_element.toString().replace(':', '');
+      y_element = y_element.toString().replace(':', '');
+
+      return ((parseFloat(x_element) === parseFloat(y_element)) ? 0 : ((parseFloat(x_element) > parseFloat(y_element)) ? 1 : -1));
+
     }
-    x_element = x_element.toString().replace(':',''); 
-    y_element = y_element.toString().replace(':',''); 
+  }
+
+  sortByPropertyDesc = function (property) {
+
+    return function (x, y) {
+      var x_element = x[property];
+      var y_element = y[property];
+      if (property == "duration") {
+
+        x_element = x_element.replace('m', '');
+        y_element = y_element.replace('m', '');
+
+        var res_x = x_element.split("h");
+        var res_y = y_element.split("h");
+
+        x_element = (parseInt(res_x[0]) * 60) + parseInt(res_x[1]);
+        y_element = (parseInt(res_y[0]) * 60) + parseInt(res_y[1]);
+      }
+      x_element = x_element.toString().replace(':', '');
+      y_element = y_element.toString().replace(':', '');
 
       return ((parseFloat(x[property]) === parseFloat(y[property])) ? 0 : ((parseFloat(x[property]) < parseFloat(y[property])) ? 1 : -1));
 
+    }
   }
-}
 
 
 }
